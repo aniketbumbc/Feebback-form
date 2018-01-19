f5df80ef7743f24bd236e39bd2d84e441e0fdcb4
aniket umbc <aniket.umbc@gmail.com>
2018-01-19 08:36:39 +0530

    Form add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ onSubmit(form:any):void{
+   console.log('The Value Submitted:',form);
+ }
 }
